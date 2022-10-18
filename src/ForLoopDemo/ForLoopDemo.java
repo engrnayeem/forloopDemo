c18686a0c25a0429d61c3467a487bf5e71d4dd31
unknown <engr.mohammadjannatulnayeem@gmail.com>
2022-10-18 20:45:10 +0600

    code uploaded

---
+++ src/ForLoopDemo/ForLoopDemo.java
@@ -0,0 +1,25 @@
+
+package ForLoopDemo;
+
+/**
+ *
+ * @author nayeem
+ */
+public class ForLoopDemo {
+    public static void main(String[]args)
+    {
+    
+    for(int x=1;x<=10;x++)
+    {
+      
+     System.out.print(x+"   ");
+        
+    }
+    
+    
+    
+    
+    }
+    
+    
+}
